dd4450eedb3bcea75b00e6777785e873f7433c38
Nikita Elfimov <en@atls.team>
2025-06-07 11:28:17 +0300

    feat(convention): change baseUrl to custom domain (#56)

--- convention/docusaurus.config.ts
+++ convention/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://atls.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'convention',
+  baseUrl: '',
 
   organizationName: 'atls',
   projectName: 'convention',
